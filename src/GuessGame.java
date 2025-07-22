2b731257ed9e201329ea562dc5df33503406aa43
Yaqeen <yaqeen.alabdali@student.manchester.ac.uk>
2025-07-22 08:39:00 +0400

    asking the user if he wants to play again

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -2,40 +2,56 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Random;
 import java.util.Scanner;
-public class GuessGame {
-    int secretNumber;
 
-    public guessGame() {
+public class GuessGame {
+    private int secretNumber;
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.play();
+
+        System.out.println("Do you want to play again?");
+        String playAgain = game.scanner.next();
+
+        while (playAgain.equalsIgnoreCase("yes") || playAgain.equalsIgnoreCase("y")) {
+            game.play();
+            System.out.println("Do you want to play again?");
+            playAgain = game.scanner.next();
+        }
+
+        System.out.println("Thanks for playing!");
+    }
+
+    public void play() {
+        guessGame();
+        start();
+    }
+
+    public void guessGame() {
         Random randomNumber = new Random();
-        this.secretNumber = randomNumber.nextInt(100) + 1;
+        secretNumber = randomNumber.nextInt(100) + 1;
     }
 
     public boolean checkGuess(int userGuess) {
-
-        if (userGuess == secretNumber) {
-            return true;
-        } else {
-            return false;
-        }
+        return userGuess == secretNumber;
     }
 
     public void start() {
-        Scanner scanner = new Scanner();
-        int tries = 0;
-        for (tries < 5; tries++ ) {
-            System.out.println("Guess the number: ");
+        for (int tries = 0; tries < 5; tries++) {
+            System.out.println("Guess the number (1-100): ");
             int userGuess = scanner.nextInt();
-            if (userGuess == secretNumber) {
-                System.out.println("You got it ");
+
+            if (checkGuess(userGuess)) {
+                System.out.println("You got it!");
                 return;
             } else if (userGuess < secretNumber) {
-                System.out.println("Too low, try again ");
-            } else if (userGuess > secretNumber) {
-                System.out.println("Too high, try again ");
+                System.out.println("Too low, try again.");
+            } else {
+                System.out.println("Too high, try again.");
             }
-
         }
-        System.out.println("the correct answer was " + secretNumber);
 
+        System.out.println("The correct answer was: " + secretNumber);
     }
 }
